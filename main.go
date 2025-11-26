38bac50e850060bd02d3291c20f7a911d97e2c51
Ximo <joaquin.rovira97@gmail.com>
2025-11-26 17:56:33 +0100

    .

--- main.go
+++ main.go
@@ -76,8 +76,8 @@ func main() {
 
 	middlewares = append(
 		middlewares,
-		mdw.Countdown(TokenService),
 		mdw.Auth(TokenService),
+		mdw.Countdown(TokenService),
 	)
 	mux.Handle("/", mdw.Apply(cache.NewCachedFileServer(root), middlewares...))
 
